45d244894be80a4f532b3c9b7fc93149f193d07d
Jeffry Langford <jblangford97@gmail.com>
2022-04-15 23:52:57 -0500

    slight changes

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,17 @@
-var startQuizBtn = document.querySelector("#start-quiz")
+var startQuizBtn = document.querySelector("#startquiz");
+
+console.log(startQuizBtn);
 
 function startQuiz () {
-    
-}
+    //take the audience to the first question with answer choices
+    //probably with an if loop
+    alert("button clicked");
+};
+
+//use if loops to take the audience through the quiz:
+//if correct: display "Correct!" then go to next question
+//if incorrect: 
+
+//store high scores on local storage after the quiz has been completed
 
 startQuizBtn.addEventListener("click", startQuiz);
