a0ff1020de44d7f6d718bba8e57608c077cff5d6
Jeffry Langford <jblangford97@gmail.com>
2022-04-20 18:59:08 -0500

    a lot of things

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ var entryEl = document.getElementById("entry");
 var questionsEl = document.getElementById("questions");
 var answerEl = document.getElementById("answer");
 var endPromptEl = document.getElementById("endprompt");
+var viewScoresEl = document.getElementById("view-scores");
+var viewScoresLink = document.getElementById("score-check");
 var timeLeft = 60;
 var timeInterval;
 var questionCounter = 0;
@@ -89,17 +91,15 @@ function questionRender () {
 
 function clickHandler(event) {
     if (event.target.tagName === "BUTTON") {
-        //answerEl.innerHTML = "";
-        var answerP = document.createElement("p");
         if (event.target.textContent === questionList[questionCounter].correctAnswer) {
-            answerP.textContent = "Correct!";
-            questionsEl.append(answerP);
+            answerEl.innerHTML = "Correct!";
             console.log("correct");
+            answerEl.classList.remove("hide");
         }
         else {
-            answerP.textContent = "Incorrect! 10s have been deducted from your total time.";
-            questionsEl.append(answerP);
+            answerEl.innerHTML = "Incorrect! 10 seconds have been deducted from your time.";
             console.log("false");
+            answerEl.classList.remove("hide");
             timeLeft = timeLeft - 10;
         }
         questionCounter = questionCounter + 1;
@@ -107,39 +107,91 @@ function clickHandler(event) {
     }
 }
 
+function scoreHandler(event) {
+    event.preventDefault();
+    var user = document.getElementById("initials").value.trim();
+    var storage = localStorage.getItem("initials");
+    if (storage) {
+        localStorage.setItem("initials", JSON.stringify(storage + user));
+    } else {
+        localStorage.setItem("initials", JSON.stringify(user));
+    }
+    viewScores();
+}
+
 function endQuiz() {
+    answerEl.classList.add("hide");
     questionsEl.classList.add("hide");
     endPromptEl.classList.remove("hide");
     var endPromptP = document.createElement("p");
     //var correctQuestions = ; //TODO: find a way to store how many correct questions the audience answered 
+    
     var finalScore = timeLeft /*+ correctQuestions*/;
     endPromptP.textContent = "Your final score is " + finalScore + ".";
     endPromptEl.append(endPromptP);
     clearInterval(timeInterval);
+
     var endPromptInput = document.createElement("input");
+    endPromptInput.setAttribute("id", "initials");
     endPromptEl.append(endPromptInput);
+
+    var endPromptSubmitScore = document.createElement("button");
+    endPromptSubmitScore.classList.add("btn-primary", "btn", "m-2", "btn-mw-10");
+    endPromptSubmitScore.textContent = "Submit Score";
+    endPromptEl.append(endPromptSubmitScore);
+
+    var lineBreak = document.createElement("br");
+    endPromptEl.append(lineBreak);
+
+    endPromptSubmitScore.addEventListener("click", scoreHandler);
+}
+
+function viewScores(event = null) {
+    if (event != null) {
+        event.preventDefault();
+    }
+    questionsEl.classList.add("hide");
+    answerEl.classList.add("hide");
+    endPromptEl.classList.add("hide");
+    var viewScoresH3 = document.createElement("h3");
+    viewScoresH3.textContent = "High Scores";
+    viewScoresEl.append(viewScoresH3);
+    viewScoresEl.classList.remove("hide");
+    var initialStorage = localStorage.getItem("initials");
+    console.log(initialStorage);
+
     var endPromptGoBack = document.createElement("button");
     endPromptGoBack.classList.add("btn-primary", "btn", "m-2", "btn-mw-10");
+    endPromptGoBack.setAttribute("id", "go-back");
     endPromptGoBack.textContent = "Go Back";
-    endPromptEl.append(endPromptGoBack);
-    var endPromptViewHighScores = document.createElement("button");
-    endPromptViewHighScores.classList.add("btn-primary", "btn", "m-2", "btn-mw-10");
-    endPromptViewHighScores.textContent = "View High Scores";
-    endPromptEl.append(endPromptViewHighScores);
+    endPromptGoBack.addEventListener("click", goBack);
+    viewScoresEl.append(endPromptGoBack);
+
+    var clearScoresBtn = document.createElement("button");
+    clearScoresBtn.classList.add("btn-primary", "btn", "m-2", "btn-mw-10");
+    clearScoresBtn.textContent = "Clear Scores";
+    viewScoresEl.append(clearScoresBtn);
+    clearScoresBtn.addEventListener("click", clearScores);
+    //get local storages
+    //append local storages and style it
 }
+
+function clearScores(event) {
+    event.preventDefault;
+    localStorage.clear();
+    goBack();
+}
+
+function goBack () {
+    window.location.reload();
+}
+
 //add score up above
 
 // create user object from submission
-/*var user = {
-    firstName: firstNameInput.value.trim(),
-  };
-
-  // set new submission to local storage 
-  localStorage.setItem("user", JSON.stringify(user));
-  console.log(firstNameInput.value + " " + lastNameInput.value + " " + emailInput.value + " " + passwordInput.value);
-*/
 
 //store high scores on local storage after the quiz has been completed
 
-questionsEl.addEventListener("click", clickHandler)
+viewScoresLink.addEventListener("click", viewScores);
+questionsEl.addEventListener("click", clickHandler);
 startQuizBtn.addEventListener("click", startQuiz);
